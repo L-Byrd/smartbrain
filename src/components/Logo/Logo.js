34c4c9858e9e586490e9825843ce79478dd21773
maybeyahweh <lebjr91@gmail.com>
2021-11-23 11:49:43 -0600

    smartbrain app that detects faces using the clarifai api, allows user to register and login with the smart-brain-api project

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
+import './Logo.css'
+const Logo = () => {
+    return (
+        <div style={{display:'flex',justifyContent:'flex-start'}}className='ma4 mt0 br2 shadow'>
+            <Tilt className='br2 shadow' tiltMaxAngleX='55' tiltMaxAngleY='55'>
+                <div className='pa3'><img style={{paddingTop: '5px'}} alt ='logo' src={brain}></img></div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
