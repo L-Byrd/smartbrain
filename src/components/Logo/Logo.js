bc833ca225712b965903db4e53793b67df60a228
maybeyahweh <lebjr91@gmail.com>
2021-11-23 13:05:37 -0600

    added profile icon and changed signout functionality to be within profile icon dropdown, fixed routing on sign out

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import brain from './brain.png'
 import './Logo.css'
 const Logo = () => {
     return (
-        <div style={{display:'flex',justifyContent:'flex-start'}}className='ma4 mt0 br2 shadow'>
+        <div style={{display:'flex',justifyContent:'flex-start'}}className='ma4 mt0 br2'>
             <Tilt className='br2 shadow' tiltMaxAngleX='55' tiltMaxAngleY='55'>
                 <div className='pa3'><img style={{paddingTop: '5px'}} alt ='logo' src={brain}></img></div>
             </Tilt>
